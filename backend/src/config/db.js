cf5513f764ce91922698f051205e1b7e597d5feb
Anubis1030 <anujdhungana6@gmail.com>
2025-07-02 10:11:03 +0545

    creating the admin role base code

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,9 +1,28 @@
 import mongoose from "mongoose";
-
+import User from "../models/User.js";
 const connectDB = async () => {
     try {
        await mongoose.connect(process.env.MONGO_URI);
        console.log("Database connected");
+
+       const admin = await User.findOne({email:"admin@gmail.com"})
+
+       console.log(admin);
+
+       if(!admin){
+       await User.create({
+            name:"admin",
+            email:"admin@gmail.com",
+            password:"admin",
+            phone:1234567890,
+            role:"admin"
+        });
+
+        console.log("admin created");
+       }else{
+        console.log("admin already exist");
+       }
+
     } catch (error) {
         console.log(error);
         
--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -3,11 +3,33 @@ import userServices from "../services/userServices.js"
 
 const createUser = async(req, res) => {
 
-    const { name, email, password, phone } = req.body
+    try{
+
+    const { name, email, password, phone,confirmPassword } = req.body
+
+    if(!name){
+        return res.send("name is required")
+    }
+    if(password){
+        return res.send("password is required")
+
+    }
+
+    if (password !== confirmPassword) {
+        return res.send("password and confirm password should be same")
+    }
  const data = await userServices.createUser(req.body)
- console.log(data);
+ 
 
  res.send(data)
+}
+catch(err){
+    console.log(err);
+    res.send(400).json({
+        message:"something went wrong",
+        error:err
+    })
+}
 }
 
 export {createUser}
